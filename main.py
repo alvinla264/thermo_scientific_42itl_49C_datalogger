e345284b2dc4daeb665c5097ba53bfd5cd870700
Alvin La <laa5@uci.edu>
2024-05-09 19:09:29 -0700

    fixed get_data function and added test functions in main

--- main.py
+++ main.py
@@ -1,32 +1,24 @@
-import serial.tools.list_ports
-import serial.serialutil
-import serial.serialwin32
-from datetime import datetime as dt
-def get_com_ports():
-    com_ports = []
-    ports = serial.tools.list_ports.comports()
-    for port, desc, _ in ports:
-        com_ports.append(port)
-    return com_ports
-
-def open_serial_port(com_port, baudrate):
-    try:
-        # Open the serial port
-        ser = serial.serialwin32.Serial(com_port, baudrate)
-        print(f"Serial port {com_port} opened successfully.")
-        return ser
-    except serial.serialutil.SerialException as e:
-        print(f"Failed to open serial port {com_port}: {e}")
-        return None
-# Example usage:
-if __name__ == "__main__":
-    com_list = get_com_ports()
-    instrument_serial_info = {"42i-TL" : {"com_port": "",
-                                          "baudrate" : 0,
-                                          "avaliable_baudrate": [1200, 2400 , 4800, 9600 , 19200 , 38400 , 57600 , 115200]},
-                              "49C":  {"com_port": "",
-                                          "baudrate" : 0,
-                                          "avaliable_baudrate" : [1200, 2400 , 4800, 9600]}}
+'''
+File: main.py
+Author: Alvin La
+Description: This file obtains data from the 42i-TL and 49C Thermo Scientific Instrument. Spectifiically
+NoX No2 No from 42i-TL and O3 from 49C. Datalogs every one second.
+'''
+import serial_func as sf
+from datetime import datetime as dt, timedelta as td
+from openpyxl import Workbook
+instrument_serial_info = {"42i-TL" : {"com_port": "",
+                                      "baudrate" : 0, 
+                                      "avaliable_baudrate": [1200, 2400 , 4800, 9600 , 19200 , 38400 , 57600 , 115200],
+                                      "measurements" : ["no", "no2", "nox"],
+                                      "serial": None},
+                          "49C":  {"com_port": "",
+                                   "baudrate" : 0,
+                                   "avaliable_baudrate" : [1200, 2400 , 4800, 9600],
+                                   "measurements" : ["o3"],
+                                   "serial": None}}
+def main():
+    com_list = sf.get_com_ports()
     for instrument in instrument_serial_info.keys():
         valid_com_port = False
         valid_baudrate = False
@@ -57,9 +49,120 @@ if __name__ == "__main__":
     print()
     for instrument in instrument_serial_info:
         print(instrument + ":", instrument_serial_info[instrument]["com_port"], instrument_serial_info[instrument]['baudrate'])
-    ser_42i = open_serial_port(instrument_serial_info["42i-TL"]["com_port"], instrument_serial_info["42i-TL"]["baudrate"])
-    if ser_42i:
-        print("Success")
-        ser_42i.write(b"Hello World\n")
-    else:
-        print("Fail")
+    for instrument in instrument_serial_info:
+        ser_temp = sf.open_serial_port(instrument_serial_info[instrument]['com_port'], instrument_serial_info[instrument]["baudrate"])
+        if not ser_temp:
+            print("Unable to open Serial port for", instrument)
+            for instrument in instrument_serial_info:
+                if instrument_serial_info[instrument]["serial"]:
+                    instrument_serial_info[instrument]["serial"].close()
+            return
+        else:
+            instrument_serial_info[instrument]["serial"] = ser_temp
+    start_time = dt.now()
+    second_timer = dt.now()
+    file_name = f'{start_time.date()}_{start_time.time()}.xlsx'
+    instrument_data = {
+        "42i-TL" : [],
+        "49C" : []
+    }
+    #checks if 3 hours has passed since sampling duration is 3 hours
+    while dt.now() < start_time + td(hours=3):
+        #checks if a second has passed
+        if dt.now() - second_timer >= td(seconds=1):
+            data_collection_time = dt.now()
+            for instrument in instrument_serial_info:
+                #writes to the instrument requesting for the data
+                data = sf.get_data(instrument_serial_info[instrument]["serial"], instrument_serial_info[instrument]["measurements"])
+                list_data = [data_collection_time]
+                for measurements in data:
+                    list_data.append(data[measurements]['value'])
+                #creates first row of headers
+                if len(instrument_data[instrument]) == 0:
+                    headers = ["Time"]
+                    for measurements in data:
+                        headers.append(f'{measurements} ({data[measurements]["value"]})')
+                    instrument_data[instrument].append(headers)    
+                instrument_data[instrument].append(list_data)
+            second_timer = dt.now()
+    wb = Workbook()
+    data_ws = {
+        "42i-TL" : wb.active,
+        "49C" : wb.create_sheet()
+    }
+    for instrument in instrument_data:
+        data_ws[instrument].title = instrument
+        for row_idx, row_data in enumerate(instrument_data[instrument], start=1):
+            for col_idx, cell_value in enumerate(row_data, start=1):
+                data_ws[instrument].cell(row=row_idx, column=col_idx, value=cell_value)
+    wb.save(file_name)
+    for instrument in instrument_serial_info:
+        instrument_serial_info[instrument]["serial"].close()
+        instrument_serial_info[instrument]['serial'] = None
+def excel_test():
+    wb = Workbook()
+    ws = {
+        "ws1" : wb.active,
+        "ws2" : wb.create_sheet()
+    }
+    for worksheet in ws:
+        ws[worksheet].title = worksheet
+    data1 = [["Column1", "Column2", "Column3"],
+             [0, 1, 2],
+             [3, 4, 5],
+             [6, 7, 8],
+             [9, 10, 11],
+             [12, 13, 14],
+             [15, 16, 17]]
+    data2 = [["Column1", "Column2"],
+             [0, 1],
+             [2, 3],
+             [4, 5],
+             [6, 7],
+             [8, 9],
+             [10, 11]]
+    for row_idx, row_data in enumerate(data1, start=1):
+        for col_idx, cell_value in enumerate(row_data, start=1):
+            ws['ws1'].cell(row=row_idx, column=col_idx, value=cell_value)
+    for row_idx, row_data in enumerate(data2, start=1):
+        for col_idx, cell_value in enumerate(row_data, start=1):
+            ws['ws2'].cell(row=row_idx, column=col_idx, value=cell_value)
+    wb.save("testfilewithdata.xlsx")
+
+def serial_test():
+    ser_42i = sf.open_serial_port("COM8", 9600)
+    if not ser_42i:
+        print("Unable to open 42iport")
+        return
+    print(sf.get_data(ser_42i, instrument_serial_info["42i-TL"]['measurements'], chr(170)))
+
+
+    # ser_42i.write(f'{chr(170)}no2\r'.encode())
+    # data = ser_42i.read_until(b'\n')
+    # ser_42i.read_until(b'\r')
+    # print(data.decode())
+    # ser_42i.write(f'{chr(170)}nox\r'.encode())
+    # data = ser_42i.read_until(b'\n')
+    # ser_42i.read_until(b'\r')
+    # print(data.decode())
+    # ser_42i.write(f'{chr(170)}no\r'.encode())
+    # data = ser_42i.read_until(b'\n')
+    # ser_42i.read_until(b'\r')
+    # print(data.decode())
+
+    
+    # ser_49C = sf.open_serial_port("COM9", 9600)
+    # if not ser_49C:
+    #     ser_49C.close()
+    #     print("Unable to open 49C port")
+    #     return
+    # print(sf.get_data(ser_49C, instrument_serial_info["49C"]["measurements"],  chr(128 + 49)))
+    # ser_49C.write(f'{chr(128 + 49)}o3\r'.encode())
+    # data = ser_49C.read_until(b'\r')
+    # print(data)
+
+
+if __name__ == "__main__":
+    #excel_test()
+    #main()
+    serial_test()
---
+++ serial_func.py
@@ -0,0 +1,42 @@
+'''
+File: serial_func.py
+Author: Alvin La
+Description: This python script contains functions that gets available com ports for your system and opens serial ports
+'''
+import serial.tools.list_ports
+import serial.serialutil
+import serial.serialwin32
+
+
+def get_com_ports():
+    com_ports = []
+    ports = serial.tools.list_ports.comports()
+    for port, desc, _ in ports:
+        com_ports.append(port)
+    return com_ports
+
+def open_serial_port(com_port, baudrate):
+    try:
+        # Open the serial port
+        ser = serial.serialwin32.Serial(com_port, baudrate)
+        print(f"Serial port {com_port} opened successfully.")
+        return ser
+    except serial.serialutil.SerialException as e:
+        print(f"Failed to open serial port {com_port}: {e}")
+        return None
+    
+def get_data(ser, compound_list, id):
+    results = {}
+    for compound in compound_list:
+        ser.write(f'{id}{compound}\r'.encode('utf-8'))
+        response = ser.read_until(b'\r').decode()
+        if response.find('\n') != -1:
+            response = response[:response.index('\n') - 1]
+        response = response.split()
+        print(response, len(response))
+
+        if len(response) == 3:
+            results[response[0]] = {"value" : float(response[1]),
+                                "unit": response[2]}
+    return results 
+
